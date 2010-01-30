41a917a8727c7a59d7b056298860745bd5627409
David Glick <davidglick@groundwire.org>
2010-01-30 00:56:49 +0000

    add tests for the Zope view resource type
    svn path=/sandbox/plone.app.skineditor/trunk/; revision=33595

--- plone/app/skineditor/tests/test_zopeview.py
+++ plone/app/skineditor/tests/test_zopeview.py
@@ -1,18 +1,22 @@
 import unittest
 import zope.component.testing
 from zope.component import provideAdapter
-from zope.app.component.hooks import setSite
+from zope.app.component.hooks import setSite, setHooks
 from zope.component.registry import Components
 from zope.interface import Interface, implements
 from zope.publisher.interfaces.browser import IBrowserRequest
 
 from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from five.customerize.zpt import TTWViewTemplate
 from plone.app.customerize.tool import ViewTemplateContainer
 from plone.app.skineditor.zopeview import ZopeViewResourceType
 
+class IDummyBrowserLayer(IBrowserRequest):
+    pass
+
 class DummyRequest(object):
-    implements(IBrowserRequest)
+    implements(IDummyBrowserLayer)
 
 class DummySite(object):
     REQUEST = DummyRequest()
@@ -31,11 +35,20 @@ class ZopeViewResourceTestLayer:
     def setUp(self):
         # global view registration
         provideAdapter(SimpleViewClass, (Interface, IBrowserRequest), provides=Interface, name='view')
+        provideAdapter(SimpleViewClass, (Interface, IDummyBrowserLayer), provides=Interface, name='view')
         # local view registration
         site = DummySite()
         setSite(site)
+        setHooks()
+        custom_zpt = TTWViewTemplate(
+            id = 'customized_view',
+            text = 'test',
+            view = SimpleViewClass,
+            permission = 'View',
+            name = 'view')
+        site.portal_view_customizations.addTemplate('customized_view', custom_zpt)
         sm = site.getSiteManager()
-        sm.registerAdapter(SimpleViewClass, (Interface, IBrowserRequest), provided=Interface, name='view')
+        sm.registerAdapter(custom_zpt, (Interface, IDummyBrowserLayer), provided=Interface, name='view')
 
     @classmethod
     def tearDown(self):
@@ -48,9 +61,42 @@ class TestZopeViewResourceType(unittest.TestCase):
     def setUp(self):
         self.rt = ZopeViewResourceType()
 
-    def test_iter(self):
+    def test_resource_order_and_attributes(self):
         resources = list(self.rt)
-        self.assertEqual(len(resources), 2)
+        self.assertEqual(len(resources), 3)
+        
+        # first resource: local registration, specific browser layer
+        res = resources[0]
+        self.assertEqual(res.name, 'view')
+        self.assertEqual(res.type, 'zopeview')
+        self.assertEqual(res.description, 'View for *')
+        self.assertEqual(res.info, 'In the database: portal_view_customizations/customized_view')
+        self.assertEqual(res.context, 'zope.interface.Interface')
+        self.assertEqual(res.layer, 'plone.app.skineditor.tests.test_zopeview.IDummyBrowserLayer')
+        self.assertEqual(res.actions, [('Edit', 'customized_view/manage_main'),
+                                       ('Remove', '/manage_delObjects?ids=customized_view')])
+
+        # second resource: global registration, specific browser layer
+        res = resources[1]
+        self.assertEqual(res.name, 'view')
+        self.assertEqual(res.type, 'zopeview')
+        self.assertEqual(res.description, 'View for *')
+        self.failUnless(res.info.startswith('On the filesystem: '))
+        self.failUnless(res.info.endswith('cmf_test_skins/test/test.pt'))
+        self.assertEqual(res.context, 'zope.interface.Interface')
+        self.assertEqual(res.layer, 'plone.app.skineditor.tests.test_zopeview.IDummyBrowserLayer')
+        self.assertEqual(res.actions, [('View', '/@@customizezpt.html?required=zope.interface.Interface,plone.app.skineditor.tests.test_zopeview.IDummyBrowserLayer&view_name=view')])
+
+        # third resource: global registration, general browser layer
+        res = resources[2]
+        self.assertEqual(res.name, 'view')
+        self.assertEqual(res.type, 'zopeview')
+        self.assertEqual(res.description, 'View for *')
+        self.failUnless(res.info.startswith('On the filesystem: '))
+        self.failUnless(res.info.endswith('cmf_test_skins/test/test.pt'))
+        self.assertEqual(res.context, 'zope.interface.Interface')
+        self.assertEqual(res.layer, 'zope.publisher.interfaces.browser.IBrowserRequest')
+        self.assertEqual(res.actions, [('View', '/@@customizezpt.html?required=zope.interface.Interface,zope.publisher.interfaces.browser.IBrowserRequest&view_name=view')])
 
 def test_suite():
     return unittest.defaultTestLoader.loadTestsFromName(__name__)
