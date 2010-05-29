50fda474b47e6acfc2eac7bad6298570b13960a1
Nathan Van Gheem <vangheem@gmail.com>
2010-05-29 17:33:30 +0000

    moved css and javascript into separate files fixes #10604
    svn path=/sandbox/plone.app.skineditor/trunk/; revision=36807

---
+++ plone/app/skineditor/browser/resources/skineditor.js
@@ -0,0 +1,71 @@
+jq(function() {
+  var updateResults = function() {
+    jq('#plone-app-skineditor-browser').load(
+      '@@plone.app.skineditor.browse #plone-app-skineditor-browser', 
+      {'name': jq('#plone-app-skineditor-name-field').val()}
+    );
+  }
+  var activateUI = function() {
+    // edit popups
+    jq('a[href*=/manage_main],a[href*=/@@customizezpt]', this).prepOverlay({
+      subtype: 'ajax',
+      filter: 'form,form+*', // everything after the 2nd table
+      formselector: 'form',
+      config: {
+        onLoad: function() {
+          // absolutize relative form actions
+          var base = this.getTrigger().data('target');
+          var content = document;
+          if(this.getContent != undefined){
+            content = this.getContent();
+          }
+          jq('form', content).each(function() {
+            var action = jq(this).attr('action');
+            if (action.charAt(0) == '/') {
+              jq(this).attr('action', base.substr(0,base.lastIndexOf('/')) + '/' + action);
+            }
+          });
+        },
+        onClose: function(){
+          var trigger = this.getTrigger().data('source').parent();
+          var title = trigger.prev('.plone-app-skineditor-resource.open');
+          trigger.load(title.attr('href'), null, function(){
+            title.nextUntil('.plone-app-skineditor-resource').remove(); //remove custom and view info
+            var dd = jq(this).find('dd');
+            title.after(dd);
+            dd.show();
+            activateUI.apply(dd);
+          });
+        }
+      }
+    });
+    // removal of customized items
+    jq('a[href*=/manage_delObjects]', this).click(function(e) {
+      e.preventDefault();
+      jq.get(jq(this).attr('href'));
+      jq(this).closest('dd').remove();
+    });
+  }
+  activateUI.apply(jq('#plone-app-skineditor-browser'));
+  // update results via AJAX
+  jq('#plone-app-skineditor-filter-form').submit(function(e) {
+    e.preventDefault();
+    updateResults();
+  });
+  // collapsible layer lists
+  jq('.plone-app-skineditor-resource').live('click', function(e) {
+    e.preventDefault();
+    var link = jq(this);
+    link.toggleClass('open');
+    var dd = jq(this).next();
+    if (dd.is('dd')) {
+      while(dd.is('dd')) { dd.slideToggle('fast'); dd = dd.next(); }
+    } else {
+      jq('<div><' + '/div>').load(jq(this).attr('href'), null, function() {
+        jq('dd', this).insertAfter(link).slideDown('fast').each(function() {
+          activateUI.apply(this);
+        });
+      });
+    }
+  });
+});
