90d57350e04da0d26a5a82b06ab8ea5a4c05b918
David Glick <davidglick@groundwire.org>
2010-06-09 19:07:26 +0000

    yay, first windmill integration test
    svn path=/sandbox/plone.app.skineditor/trunk/; revision=37129

--- plone/app/skineditor/tests/test_integration.py
+++ plone/app/skineditor/tests/test_integration.py
@@ -19,11 +19,45 @@ class SkinEditorIntegrationTestCase(WindmillTestCase):
     def afterSetUp(self):
         """Setup for each test
         """
+        ztc.utils.setupCoreSessions(self.app)
         self.setRoles(['Manager'])
         self.login_user()
 
-    def test_dummy(self):
-        import pdb; pdb.set_trace( )
+    def test_customize_logo(self):
+        client = self.wm
+        client.click(id=u'user-name')
+        # load customizer
+        client.click(link=u'Site Setup')
+        client.waits.forPageLoad(timeout=u'20000')
+        client.click(link=u'Theme Editor')
+        # go into advanced mode and customize based on the logo in the
+        # non-active "plone_images" layer (Windmill doesn't do file uploads)
+        client.click(link=u'Advanced')
+        client.click(id=u'plone-app-skineditor-name-field')
+        client.type(text=u'logo', id=u'plone-app-skineditor-name-field')
+        client.click(id=u'plone-app-skineditor-filter-button')
+        client.waits.forElement(timeout=u'', id=u'plone-app-skineditor-browser')
+        client.click(xpath=u"//a[@id='skineditor-logo.png']/dt")
+        client.waits.forElement(xpath=u"//dd[@class='plone-app-skineditor-layers']")
+        client.click(jquery=u"('a[href*=plone_images/logo.png/manage_main]')[0]")
+        client.waits.forElement(jquery=u"('#pb_1 input[value=Customize]')")
+        client.click(name=u'submit')
+        client.waits.forElement(timeout=u'', id=u'pb_2')
+        # now reload and make sure the logo has the height we expect from the
+        # customized image
+        client.refresh()
+        client.asserts.assertJS(js=u"$('#portal-logo').height() == 57")
+        # now remove the customization
+        client.click(id=u'plone-app-skineditor-name-field')
+        client.type(text=u'logo', id=u'plone-app-skineditor-name-field')
+        client.click(id=u'plone-app-skineditor-filter-button')
+        client.waits.forElement(timeout=u'', id=u'skineditor-logo.png')
+        client.click(xpath=u"//a[@id='skineditor-logo.png']/dt")
+        client.waits.forElement(xpath=u"//dd[@class='plone-app-skineditor-layers']")
+        client.click(link=u'Remove')
+        # and confirm we're back to the original height
+        client.refresh()
+        client.asserts.assertJS(js=u"$('#portal-logo').height() == 56")
 
 def test_suite():
     return unittest.defaultTestLoader.loadTestsFromName(__name__)
