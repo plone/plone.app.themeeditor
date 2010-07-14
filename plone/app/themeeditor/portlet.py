c72f7a3607f18ec8834a937d90fb7f154273b4d4
David Bain <pigeonflight@gmail.com>
2010-07-14 03:53:21 +0000

    added the beginnings of an INSTALL.txt removed redundant code from portlet.py and zopeview.py was initially added for Plone 3 compatibility but isn't needed with the monkeypatch
    svn path=/plone.app.themeeditor/trunk/; revision=37785

--- plone/app/themeeditor/portlet.py
+++ plone/app/themeeditor/portlet.py
@@ -48,14 +48,7 @@ class PortletResourceType(object):
         by_layer_precedence_and_ttwness = lambda x: (layer_precedence.index(x.required[1]), int(not ITTWViewTemplate.providedBy(x.factory)))
         regs = sorted(self.iter_portlet_registrations(), key=by_layer_precedence_and_ttwness)
        
-        try:
-            tempregInfo = templateViewRegistrationInfos(regs, mangle=False)
- 
-        # if we are using an older version of customerize
-        # mangle does not work to skip it
-        except:
-            tempregInfo = templateViewRegistrationInfos(regs)
-        for info in tempregInfo:
+        for info in templateViewRegistrationInfos(regs, mangle=False):
             required = info['required'].split(',')
             res = PortletResourceRegistration()
             res.name = info['viewname']
